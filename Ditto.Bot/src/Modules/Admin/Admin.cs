8481e71a98e2d50cd25b953d52477f9ba15e06f2
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2020-08-10 23:08:04 +0200

    Added disconnect method and fixed permission for logging method.

--- Ditto.Bot/src/Modules/Admin/Admin.cs
+++ Ditto.Bot/src/Modules/Admin/Admin.cs
@@ -1,7 +1,7 @@
-﻿using Ditto.Attributes;
+﻿using Discord.Commands;
+using Ditto.Attributes;
 using Ditto.Data.Commands;
 using Ditto.Data.Discord;
-using Google.Apis.Logging;
 using System.Threading.Tasks;
 
 namespace Ditto.Bot.Modules.Admin
@@ -17,12 +17,32 @@ namespace Ditto.Bot.Modules.Admin
         [DiscordCommand(CommandSourceLevel.All, CommandAccessLevel.Global)]
         public async Task Debug(bool enable = true)
         {
-            if(!Permissions.IsBotOwner(Context))
+            if (!Permissions.IsBotOwner(Context))
             {
                 await Context.ApplyResultReaction(CommandResult.FailedUserPermission).ConfigureAwait(false);
             }
+            else
+            {
+                Log.Setup(Log.LogToConsole, Log.LogToFile, enable);
+            }
+        }
 
-            Log.Setup(Log.LogToConsole, Log.LogToFile, enable);
+
+        [DiscordCommand(CommandSourceLevel.All, CommandAccessLevel.Global)]
+        [Alias("dc")]
+        public async Task Disconnect(bool enable = true)
+        {
+            if (!Permissions.IsBotOwner(Context))
+            {
+                await Context.ApplyResultReaction(CommandResult.FailedUserPermission).ConfigureAwait(false);
+            }
+            else
+            {
+                await Ditto.Client.DoAsync(async client =>
+                {
+                    await client.StopAsync().ConfigureAwait(false);
+                }).ConfigureAwait(false);
+            }
         }
     }
 }
