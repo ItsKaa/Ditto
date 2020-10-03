6da76207a9a7a610a0961d2df5984a5426541287
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2020-10-03 15:05:45 +0200

    Ditto.Core - Added WebExtensions.

---
+++ Ditto.Core/src/Extensions/WebExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Ditto.Extensions
+{
+    public static class WebExtensions
+    {
+        public static Uri Append(this Uri uri, params string[] paths)
+        {
+            return new Uri(paths.Aggregate(uri.AbsoluteUri, (current, path) => string.Format("{0}/{1}", current.TrimEnd('/'), path.TrimStart('/'))));
+        }
+    }
+}
