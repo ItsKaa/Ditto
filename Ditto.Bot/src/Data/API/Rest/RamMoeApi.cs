a0485a20df4ca1345effb765d50febf484d337ee
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2020-10-03 15:05:57 +0200

    Added Ram.Moe api

---
+++ Ditto.Bot/src/Data/API/Rest/RamMoeApi.cs
@@ -0,0 +1,70 @@
+﻿using Discord.Commands;
+using Ditto.Data;
+using Ditto.Extensions;
+using System;
+using System.ComponentModel;
+
+namespace Ditto.Bot.Data.API.Rest
+{
+    public class RamMoeApi : RestBase<RamMoeApi.Result>
+    {
+        public enum Type
+        {
+            
+            Cry,
+            Cuddle,
+            Hug,
+            Kiss,
+            Lewd,
+            Lick,
+            Nom,
+            Nyan,
+            Owo,
+            Pat,
+            Pout,
+            Rem,
+            Slap,
+            Smug,
+            Stare,
+            Tickle,
+            Triggered,
+            [Description("nsfw-gtn")]
+            Nsfw,
+            Potato,
+            Kermit,
+        }
+
+        public class Result
+        {
+            public string Path { get; set; }
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public bool Nsfw { get; set; }
+        }
+
+        public RamMoeApi()
+        {
+            BaseUrl = new Uri("https://rra.ram.moe");
+        }
+
+        public Result RandomImage(Type type, bool nsfw = false)
+        {
+            var typeString = (type.GetAttribute<DescriptionAttribute>()?.Description ?? type.ToString()).ToLower();
+
+            var result = Call("i/r/", new[]
+            {
+                new RestSharp.Parameter("type", typeString, RestSharp.ParameterType.GetOrPost),
+                new RestSharp.Parameter("nsfw", nsfw.ToString().ToLower(), RestSharp.ParameterType.GetOrPost),
+            });
+
+            // Absolute path.
+            if (!string.IsNullOrEmpty(result?.Path))
+            {
+                result.Path = BaseUrl.Append(result.Path).ToString();
+            }
+
+            return result;
+        }
+
+    }
+}
