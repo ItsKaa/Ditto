fc662ea234c6d0867fc900f731de573712795063
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2023-05-05 22:47:51 +0200

    Added getting/setting cache channel in Admin.

--- Ditto.Bot/src/Modules/Admin/Admin.cs
+++ Ditto.Bot/src/Modules/Admin/Admin.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Ditto.Attributes;
 using Ditto.Data.Commands;
 using Ditto.Data.Discord;
@@ -8,6 +9,22 @@ namespace Ditto.Bot.Modules.Admin
 {
     public class Admin : DiscordModule
     {
+        public static ITextChannel CacheChannel { get; private set; }
+
+        static Admin()
+        {
+            Ditto.Connected += async () =>
+            {
+                var cacheChannelConfig = await Ditto.Database.ReadAsync(uow => uow.Configs.GetGlobalCacheChannel()).ConfigureAwait(false);
+                if (ulong.TryParse(cacheChannelConfig?.Value, out ulong channelId))
+                {
+                    CacheChannel = await Ditto.Client.DoAsync(async x =>
+                        await x.GetChannelAsync(channelId, new RequestOptions() { RetryMode = RetryMode.AlwaysRetry }).ConfigureAwait(false)
+                    ).ConfigureAwait(false) as ITextChannel;
+                }
+            };
+        }
+
         [DiscordCommand(CommandSourceLevel.Guild, CommandAccessLevel.LocalAndParents)]
         public override Task _()
         {
@@ -42,6 +59,30 @@ namespace Ditto.Bot.Modules.Admin
                 {
                     await client.StopAsync().ConfigureAwait(false);
                 }).ConfigureAwait(false);
+            }
+        }
+
+        [DiscordCommand(CommandSourceLevel.Guild, CommandAccessLevel.Local)]
+        public async Task SetCache(ITextChannel textChannel)
+        {
+            if (!Permissions.IsBotOwner(Context))
+            {
+                await Context.ApplyResultReaction(CommandResult.FailedUserPermission).ConfigureAwait(false);
+            }
+            if (!await Permissions.CanBotSendMessages(textChannel).ConfigureAwait(false))
+            {
+                await Context.ApplyResultReaction(CommandResult.FailedBotPermission).ConfigureAwait(false);
+            }
+            else
+            {
+                await Ditto.Database.DoAsync(uow =>
+                {
+                    uow.Configs.SetGlobalCacheChannel(textChannel);
+                }).ConfigureAwait(false);
+
+                CacheChannel = textChannel;
+
+                await Context.ApplyResultReaction(CommandResult.Success).ConfigureAwait(false);
             }
         }
     }
