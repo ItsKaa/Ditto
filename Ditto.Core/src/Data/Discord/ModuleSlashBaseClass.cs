e77cf69f8184aa26c31b81902fcfffe187d26b16
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2023-06-17 21:24:20 +0200

    Added Discord modules for slash command classes.

---
+++ Ditto.Core/src/Data/Discord/ModuleSlashBaseClass.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Interactions;
+using System;
+
+namespace Ditto.Data.Discord
+{
+    public abstract class ModuleSlashBaseClass : InteractionModuleBase, IDisposable
+    {
+        protected bool IsDisposing { get; private set; }
+        private InteractionService InteractionService { get; }
+
+        protected ModuleSlashBaseClass(InteractionService interactionService)
+        {
+            InteractionService = interactionService;
+        }
+
+        public void Dispose()
+        {
+            if (!IsDisposing)
+            {
+                IsDisposing = true;
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+    }
+}
