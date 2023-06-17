58cfa64cf4c04f84f0165f401b7288dbc3d27e79
ItsKaa <31796925+ItsKaa@users.noreply.github.com>
2023-06-17 21:24:20 +0200

    Added Discord modules for slash command classes.

---
+++ Ditto.Core/src/Data/Discord/DiscordSlashModule.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Interactions;
+
+namespace Ditto.Data.Discord
+{
+    public abstract class DiscordSlashModule : ModuleSlashBaseClass
+    {
+        protected DiscordSlashModule(InteractionService interactionService)
+            : base(interactionService)
+        {
+        }
+    }
+}
